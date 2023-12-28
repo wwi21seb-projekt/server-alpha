7ce7f4efce3f9d2f8bb377991d64664cf3264bfb
930C <93C@pm.me>
2023-12-28 02:31:13 +0100

    Refactor scripts and update README
    The commit refactors several scripts, introduces a new 'clean_db.sh' script, and deletes the 'clean.sh' script. Moreover, the README has been updated with a 'Getting Started' section. These changes improve script usability, clarity and make running, building, and cleaning tasks easier and more intuitive.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,11 +4,9 @@
 # Enable BuildKit
 export DOCKER_BUILDKIT=1
 
-# Get the current directory of the script
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Navigate to the root directory
-cd "${SCRIPT_DIR}/.." || exit
+#
+BASE_DIR=$(dirname "$0")/..
+cd "$BASE_DIR" || exit
 
 # Build the Docker image
 docker build -t server-alpha -f build/package/Dockerfile .
