757b86491ca9f1fafe70a7417b32bfc71843cc68
abramsmax <max.abrams27@gmail.com>
2024-02-02 12:02:13 +0100

    Go Doc Comments

--- internal/utils/url_keys.go
+++ internal/utils/url_keys.go
@@ -1,30 +1,13 @@
 package utils
 
 const (
-	// SubscriptionIdKey is the key for subscription ID used in routing parameters.
-	SubscriptionIdKey = "subscriptionId"
-
-	// UsernameKey is the key for username used in routing parameters.
-	UsernameKey = "username"
-
-	// UsernameParamKey is the key for username used in query parameters.
-	UsernameParamKey = "username"
-
-	// OffsetParamKey is the key for offset used in pagination query parameters.
-	OffsetParamKey = "offset"
-
-	// LimitParamKey is the key for limit used in pagination query parameters.
-	LimitParamKey = "limit"
-
-	// PostIdParamKey is the key for post ID used in routing parameters.
-	PostIdParamKey = "postId"
-
-	// FeedTypeParamKey is the key for feed type used in query parameters.
-	FeedTypeParamKey = "feedType"
-
-	// SubscriptionTypeParamKey is the key for subscription type used in query parameters.
-	SubscriptionTypeParamKey = "type"
-
-	// QueryParamKey is the key for a generic query used in query parameters.
-	QueryParamKey = "q"
+	SubscriptionIdKey        = "subscriptionId" // SubscriptionIdKey is the key for subscription ID used in routing parameters.
+	UsernameKey              = "username"       // UsernameKey is the key for username used in routing parameters.
+	UsernameParamKey         = "username"       // UsernameParamKey is the key for username used in query parameters.
+	OffsetParamKey           = "offset"         // OffsetParamKey is the key for offset used in pagination query parameters.
+	LimitParamKey            = "limit"          // LimitParamKey is the key for limit used in pagination query parameters.
+	PostIdParamKey           = "postId"         // PostIdParamKey is the key for post ID used in routing parameters.
+	FeedTypeParamKey         = "feedType"       // FeedTypeParamKey is the key for feed type used in query parameters.
+	SubscriptionTypeParamKey = "type"           // SubscriptionTypeParamKey is the key for subscription type used in query parameters.
+	QueryParamKey            = "q"              // QueryParamKey is the key for a generic query used in query parameters.
 )
