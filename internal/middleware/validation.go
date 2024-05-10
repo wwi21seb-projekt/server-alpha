b079d832f3093ab0f9bea12cc0b882325029fef5
Kevin Rieger <kevin.rieger@sap.com>
2024-05-10 16:33:40 +0200

    :(

--- internal/middleware/validation.go
+++ internal/middleware/validation.go
@@ -12,19 +12,19 @@ import (
 func ValidateAndSanitizeStruct(obj interface{}) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		if err := c.ShouldBindJSON(obj); err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, schemas.BadRequest)
+			c.AbortWithStatusJSON(http.StatusBadRequest, &schemas.ErrorDTO{Error: *schemas.Unauthorized})
 			return
 		}
 		validator := validators.GetValidator()
 		// Sanitize the data
 		if err := validator.SanitizeData(obj); err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, schemas.BadRequest)
+			c.AbortWithStatusJSON(http.StatusBadRequest, &schemas.ErrorDTO{Error: *schemas.Unauthorized})
 			return
 		}
 
 		if err := validator.Validate.Struct(obj); err != nil {
 			// Handle validation errors as before
-			c.AbortWithStatusJSON(http.StatusBadRequest, schemas.BadRequest)
+			c.AbortWithStatusJSON(http.StatusBadRequest, &schemas.ErrorDTO{Error: *schemas.Unauthorized})
 			return
 		}
 		// Set the sanitized object in the context
